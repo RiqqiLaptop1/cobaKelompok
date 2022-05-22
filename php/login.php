075d889b3b40f5ea83d31046ab971964ae076e62
RiqqiLaptop1 <riqqia--@gmail.com>
2022-05-22 20:17:29 +0700

    menggunakan bootstrap pada login

--- php/login.php
+++ php/login.php
@@ -20,36 +20,45 @@ if (isset($_POST['masuk'])) {
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+  <link rel="stylesheet" href="..\bootstrap-4.6.1-dist\css\bootstrap.css">
+
   <title>Login</title>
 </head>
 
 <body>
-  <h3>Form login Toko Ali 2</h3>
 
   <?php if (isset($login['error'])) : ?>
-    <p style="color: red; font-style: italic;"><?= $login['pesan']; ?></p>
+    <p><?= $login['pesan']; ?></p>
   <?php endif ?>
+  <br><br><br><br><br>
+  <div class="container ">
+    <div class="row">
+      <div class="col-md-4"></div>
+      <div class="col-md-4  card border-dark bg-light mb-3 p-3">
+        <div class="text-center">
+          <img src="../img/zero-two.jpg" class="img-thumbnail rounded-circle" width="100" height="100">
+          <h1>Toko Ali 2</h1>
+        </div>
+        <form action="" method="POST">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control " id="username" name="username" autofocus required autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" required>
+          </div>
+          <button class="btn btn-primary mx-auto d-block" type="submit" name="masuk">Masuk</button>
+        </form>
+      </div>
+      <div class="col-md-4"></div>
+    </div>
+  </div>
 
-  <form action="" method="POST">
-    <ul>
-      <li>
-        <label>
-          Username
-          <input type="text" name="username" autofocus required autocomplete="off">
-        </label>
-      </li>
-      <li>
-        <label>
-          Password
-          <input type="password" name="password" required>
-        </label>
-      </li>
-      <li>
-        <button type="submit" name="masuk">Masuk</button>
-      </li>
-    </ul>
-  </form>
+  <script src="..\bootstrap-4.6.1-dist\js\jquery-3.6.0.min.js"></script>
+  <script src="..\bootstrap-4.6.1-dist\js\bootstrap.bundle.js"></script>
 </body>
 
 </html>
